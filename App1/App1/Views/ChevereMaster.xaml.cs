28185d4c876ee8d82685bb28156912ccd0d6c912
Roger <esurmay@gmail.com>
2017-05-26 08:46:26 +0200

    Primera carga de la App

---
+++ App1/App1/Views/ChevereMaster.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChevereMaster : ContentPage
+    {
+        //public ListView ListView => ChevereMenuItems;
+        //public ListView ListView { get { return listView; } }
+
+        //ListView listView;
+
+        public ChevereMaster()
+        {
+            InitializeComponent();
+            BindingContext = new ChevereMasterViewModel();
+        }
+
+
+
+        class ChevereMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<ChevereMenuItem> ChevereMenuItems { get; }
+            public ChevereMasterViewModel()
+            {
+                ChevereMenuItems = new ObservableCollection<ChevereMenuItem>(new[]
+                {
+                    new ChevereMenuItem { Id = 0, Title = "Page 1" },
+                    new ChevereMenuItem { Id = 1, Title = "Page 2" },
+                    new ChevereMenuItem { Id = 2, Title = "Page 3" },
+                    new ChevereMenuItem { Id = 3, Title = "Page 4" },
+                    new ChevereMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        }
+    }
+}
