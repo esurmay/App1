9bfcb75377a668d668a3b34581ec09e8c18be6f9
Roger E Surmay <esurmay@gmail.com>
2017-11-06 17:48:22 +0100

    Update AppDelegate.cs
    test audio in background

--- App1/App1.iOS/AppDelegate.cs
+++ App1/App1.iOS/AppDelegate.cs
@@ -9,10 +9,19 @@ namespace LevantateChevere.iOS
 	{
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-            global::Xamarin.Forms.Forms.Init();
-            UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
-            UIApplication.SharedApplication.SetStatusBarHidden(false, false);
-            LoadApplication(new App());
+		    global::Xamarin.Forms.Forms.Init();
+		    
+		    //test audio in Background
+		    NSError error;
+		    AVAudioSession instance = AVAudioSession.SharedInstance();
+		    instance.SetCategory(new NSString("AVAudioSessionCategoryPlayback"), AVAudioSessionCategoryOptions.MixWithOthers, out error);
+		    instance.SetMode(new NSString("AVAudioSessionModeDefault"), out error);
+		    instance.SetActive(true, AVAudioSessionSetActiveOptions.NotifyOthersOnDeactivation, out error);
+		    //end test audio in Background
+
+		    UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
+		    UIApplication.SharedApplication.SetStatusBarHidden(false, false);
+		    LoadApplication(new App());
 
 			return base.FinishedLaunching(app, options);
 		}
